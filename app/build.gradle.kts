3c2a5c76436fddc2f121c693d1ca89a4414a9931
tonymushah <tonyramanantsalama@gmail.com>
2025-06-26 02:34:13 +0300

    repo changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     testImplementation(libs.junit)
 
     // This dependency is used by the application.
-    implementation(libs.guava)
+    // implementation(libs.guava)
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -32,5 +32,5 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.App"
+    mainClass = "mg.dirk.vote_system.App"
 }
